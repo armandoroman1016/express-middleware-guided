b7a4305fe0394e1b2ccee4bc01a07f7115e63f62
Armando Roman <armandoroman1016@gmail.com>
2019-09-04 10:50:52 -0700

    suppliers routes gets by name

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express')
-
+const productsRouter = require('./products/products_router')
+const suppliersRoutes = require('./suppliers/suppliers_router')
 const server = express()
+
+server.use('/products', productsRouter)
+server.use('/suppliers', suppliersRoutes)
 
 server.get('/', (req, res) => {
     res.status(200).json({message: 'welcome'})
 })
+
 
 module.exports = server
